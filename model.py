d12089ae8332eb2a12b721132dd810b8ec1f9883
Erebos <erebos@rams.colostate.edu>
2019-11-25 06:04:40 +0800

    fix cache bugs and grid for threold

--- model.py
+++ model.py
@@ -16,7 +16,6 @@ DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class quora_detector(nn.Module):
     def __init__(self, vec_dim, fea_num):
         super(quora_detector, self).__init__()
-        #self.embeds = 
         self.rnn = nn.LSTM(input_size = vec_dim, hidden_size = 64, batch_first = True, bidirectional = True)
         self.conv = nn.Conv1d(in_channels=1, out_channels=64, kernel_size=1)
         self.stat_dense = nn.Linear(fea_num, 64)
@@ -60,6 +59,7 @@ class quora_detector(nn.Module):
 
         # stats features embedding(to batchsize*64)
         stat_features = self.stat_dense(stat_features)
+        stat_features = F.relu(stat_features, inplace = True)
 
         # concat two embeddings(to batchsize*128)
         output = torch.cat((context, stat_features), 1)
@@ -152,82 +152,97 @@ def predict(model, sentences, length, stats_features, targets, batch_size = 100,
         ret = np.r_[ret, res]
     return ret
 
-def train(model, train_sentences, train_stats_features, train_targets, index2word, word2vec, valid_train_rate = 0.1, learning_rate = 0.001, batch_size = 100, optimizer = "Adam", iterations = 100):
+def train(model, train_sentences, train_stats_features, train_targets, index2word, word2vec, valid_train_rate = 0.1, learning_rate = 0.001, batch_size = 100, optimizer = "Adam", iterations = 100, threshold = 0.5, training = True):
     vec_dim = word2vec['how'].shape[0]
     train_length = torch.tensor(train_stats_features)[:,-1]
     train_stats_features = torch.tensor(train_stats_features).float()
     train_targets = torch.tensor(train_targets).float()
-    
+        
     train_sentences, train_length, train_stats_features, train_targets, \
         valid_sentences, valid_length, valid_stats_features, valid_targets = \
         split_train_valid(train_sentences, train_length, train_stats_features, train_targets, valid_train_rate = valid_train_rate)
 
-    if optimizer == "SGD":
-        optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
-    else:
-        optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
-    model.to(DEVICE)
+    if training:
+        if optimizer == "SGD":
+            optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+        else:
+            optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
 
-    print("Training will on GPU" if DEVICE == torch.device("cuda:0") else "Training will on CPU")
+        # put model on GPU and parallelize
+        model.to(DEVICE)
+        model = nn.DataParallel(model)
 
-    losses = []
-    train_errors = []
-    test_errors = []
+        print("Training will on GPU" if DEVICE == torch.device("cuda:0") else "Training will on CPU")
 
-    model.train()
-    train_start_time = time.perf_counter()
-    for epoch in range(iterations):
-        for k in range((len(train_sentences)-1)//batch_size + 1):
-            #s = time.perf_counter()
-            optimizer.zero_grad()
-            model.zero_grad()
+        losses = []
+        train_errors = []
+        test_errors = []
 
-            batch_L = train_length[k*batch_size:(k+1)*batch_size]
-            batch_L, idx = torch.sort(batch_L, descending = True)
-            batch_L = batch_L.to(DEVICE)
-            batch_sentences = train_sentences[k*batch_size:(k+1)*batch_size]
-            batch_X = []
-            for sentence in batch_sentences:
-                vec = []
-                for index in sentence:
-                    if index2word[index] not in word2vec.keys():
-                        vec.append(np.zeros(vec_dim))
-                    else:
-                        vec.append(word2vec[index2word[index]])
-                batch_X.append(torch.tensor(vec))
-            batch_X = torch.nn.utils.rnn.pad_sequence(batch_X, batch_first=True).float().to(DEVICE)
-            batch_Y = train_targets[k*batch_size:(k+1)*batch_size][idx].to(DEVICE)
-            batch_F = train_stats_features[k*batch_size:(k+1)*batch_size][idx].to(DEVICE)
+        model.train()
+        train_start_time = time.perf_counter()
+        for epoch in range(iterations):
+            for k in range((len(train_sentences)-1)//batch_size + 1):
+                s = time.perf_counter()
+                # free unreferenced cuda memory and put gradient zero
+                torch.cuda.empty_cache()
+                optimizer.zero_grad()
 
-            logit = model.forward(batch_X, batch_L, batch_F)
-            loss = F.cross_entropy(logit, batch_Y.long())
-            
-            loss.backward()
-            optimizer.step()
-            #e = time.perf_counter()
-            #print("time cost this batch: {}".format(e-s), end = "")
-            print("loss in epoch {} and batch {}: {}".format(epoch+1, k+1, loss))
-            losses.append(loss)
-        #print("last loss in epoch {}: {}".format(epoch+1, loss))
-    train_stop_time = time.perf_counter()
-    print("Total Training Time: {} seconds".format(train_stop_time - train_start_time))
-    plt.plot(range(1, 1 + len(losses)), losses)
-    plt.xlabel("iterations")
-    plt.ylabel("loss function")
-    plt.show()
-    plt.savefig("loss.jpg")
+                batch_L = train_length[k*batch_size:(k+1)*batch_size]
+                batch_L, idx = torch.sort(batch_L, descending = True)
+                batch_L = batch_L.to(DEVICE)
+                batch_sentences = train_sentences[k*batch_size:(k+1)*batch_size]
+                batch_X = []
+                for sentence in batch_sentences:
+                    vec = []
+                    for index in sentence:
+                        if index2word[index] not in word2vec.keys():
+                            vec.append(np.zeros(vec_dim))
+                        else:
+                            vec.append(word2vec[index2word[index]])
+                    batch_X.append(torch.tensor(vec))
+                batch_X = torch.nn.utils.rnn.pad_sequence(batch_X, batch_first=True).float().to(DEVICE)
+                batch_Y = train_targets[k*batch_size:(k+1)*batch_size][idx].to(DEVICE)
+                batch_F = train_stats_features[k*batch_size:(k+1)*batch_size][idx].to(DEVICE)
+
+                logit = model.forward(batch_X, batch_L, batch_F)
+                loss = F.cross_entropy(logit, batch_Y.long())
+                
+                loss.backward()
+                optimizer.step()
+                e = time.perf_counter()
+                #print("time cost this batch: {}".format(e-s), end = "")
+                print("loss in epoch {} and batch {}: {}".format(epoch+1, k+1, loss))
+                losses.append(float(loss))
+            #print("last loss in epoch {}: {}".format(epoch+1, loss))
+        train_stop_time = time.perf_counter()
+        print("Total Training Time: {} seconds".format(train_stop_time - train_start_time))
+
+        # show and save loss-iteration plot
+        plt.plot(range(1, 1 + len(losses)), losses)
+        plt.xlabel("iterations")
+        plt.ylabel("loss function")
+        plt.savefig("loss.jpg")
+        #plt.show()
+
+        # save model into binary file
+        with open("./model", 'wb') as f:
+            pickle.dump(model, f)
+
     model.eval()
-
     # threshold is for toxic sentences, i.e. model will judge a sentence toxic when output_sofrmax(toxic) > threshold
-    acc = accuracy(model, valid_sentences, valid_length, valid_stats_features, valid_targets, threshold = 0.5, batch_size = batch_size)
+    eval_start = time.perf_counter()
+    acc = accuracy(model, valid_sentences, valid_length, valid_stats_features, valid_targets, threshold = threshold, batch_size = batch_size)
+    eval_end = time.perf_counter()
+    print("Total accuracy computation time: {}".format(eval_end - eval_start))
     print("Accuracy on validation set:{}".format(acc))
 
-    # save model into binary file
-    with open("./model", 'wb') as f:
-        pickle.dump(model, f)
+        
 
 if __name__ == "__main__":
     sentence2index, index2word, word2vec, train_targets, train_stats_features = getData()
-    model = quora_detector(word2vec['how'].shape[0], len(train_stats_features[0]))
-    # here [:1000] is for small data test
-    train(model, sentence2index, train_stats_features, train_targets, index2word, word2vec, valid_train_rate = 0.1, learning_rate = 0.001, batch_size = 500, optimizer = "Adam", iterations = 1)
+    for th in [0, 0.1, 0.9, 1]:
+        with open("./model", 'rb') as f:
+            model = pickle.load(f)#quora_detector(word2vec['how'].shape[0], len(train_stats_features[0]))
+        # here [:1000] is for small data test
+        train(model, sentence2index, train_stats_features, train_targets, index2word, word2vec, \
+            valid_train_rate = 0.2, learning_rate = 0.001, batch_size = 512, optimizer = "Adam", iterations = 1, threshold = th, training = False)
