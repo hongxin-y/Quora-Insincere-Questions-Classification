79b30fa509d166e3168ddac60ddf5a7f86cefa9a
Sr0121 <532835534@qq.com>
2019-11-24 18:42:29 -0500

    split positive and negative samples

--- utils.py
+++ utils.py
@@ -8,11 +8,14 @@ from gensim.models import KeyedVectors
 def get_index2word_sentence2index():
     index2word = {}  # 从索引对应到word {1: "the", 2: "table"}
     word2index = {}  # 从word到索引 {"the": 1, "table": 2}
-    sentence2index = []  # 从句子对应到向量 "the table" -> [1, 2]
-    targets = [] # 类别
+    sentence2index_1 = []  # 从句子对应到向量 "the table" -> [1, 2]
+    sentence2index_0 = []
+    targets_1 = [] # 类别
+    targets_0 = []
 
     # 字符串长度 词的数量 大写字母数量 标点符号数量 特殊词数量 句子中token的数量(向量化词长度)
-    statistics_feature = []
+    statistics_feature_1 = []
+    statistics_feature_0 = []
 
     index_count = 0
     special_words = ["stupid", "discriminat", "dictatorship", "black", "gay", "race", "penis", "hypocritical", "kill",
@@ -51,6 +54,7 @@ def get_index2word_sentence2index():
         tmp_stat_feature.append(special_words_count)
 
         # 将文本转化为索引
+        sentence = sentence.replace("can't", "can not")
         sentence = sentence.replace("'re", " are")
         sentence = sentence.replace("n't", " not")
         sentence = sentence.replace("what's", "what is")
@@ -66,11 +70,20 @@ def get_index2word_sentence2index():
 
             tmp_sentence2index.append(word2index[token])
 
-        sentence2index.append(tmp_sentence2index)
-        statistics_feature.append(tmp_stat_feature)
-        targets.append(target)
+        if target == 0:
+            sentence2index_0.append(tmp_sentence2index)
+            statistics_feature_0.append(tmp_stat_feature)
+            targets_0.append(target)
+        else:
+            sentence2index_1.append(tmp_sentence2index)
+            statistics_feature_1.append(tmp_stat_feature)
+            targets_1.append(target)
+        # sentence2index.append(tmp_sentence2index)
+        # targets.append(target)
+        # statistics_feature.append(tmp_stat_feature)
 
-    return word2index, index2word, sentence2index, statistics_feature, targets
+    return word2index, index2word, sentence2index_0, sentence2index_1, \
+           statistics_feature_0, statistics_feature_1, targets_0, targets_1
 
 
 # 生成词向量
@@ -94,8 +107,10 @@ def get_word2vec(word2index):
 # sentence2index 为 [[1, 2]]时,生成[[vec1, vec2]] 词向量维度为300,句子间无padding
 def get_index2vec(sentence2index, index2word, word2vec):
     sentenceVec = []
-    for indexList in sentence2index:
+    #for indexList in sentence2index:
+    while(len(sentence2index) > 0):
         tmpVec = []
+        indexList = sentence2index[0]
         for index in indexList:
             if index not in word2vec.keys():
                 tmpVec.append(np.zeros(300))
@@ -104,24 +119,32 @@ def get_index2vec(sentence2index, index2word, word2vec):
                 vec = word2vec[word]
                 tmpVec.append(vec)
         sentenceVec.append(tmpVec)
+        del sentence2index[0]
     return sentenceVec
 
 
 def preprocess():
-    # 处理:
-    word2index, index2word, sentence2index, statistics_feature, targets = get_index2word_sentence2index()
+    word2index, index2word, sentence2index_0, sentence2index_1, \
+    statistics_feature_0, statistics_feature_1, targets_0, targets_1 = get_index2word_sentence2index()
     with open("./data/word2index", "wb") as f:
         pickle.dump(word2index, f)
     with open("./data/index2word", "wb") as f:
         pickle.dump(index2word, f)
-    with open("./data/sentence2index", "wb") as f:
-        pickle.dump(sentence2index, f)
-    with open("./data/statistics_feature", "wb") as f:
-        pickle.dump(statistics_feature, f)
-    with open("./data/targets", "wb") as f:
-        pickle.dump(targets, f)
+    with open("./data/sentence2index_1", "wb") as f:
+        pickle.dump(sentence2index_1, f)
+    with open("./data/sentence2index_0", "wb") as f:
+        pickle.dump(sentence2index_0, f)
+    with open("./data/statistics_feature_1", "wb") as f:
+        pickle.dump(statistics_feature_1, f)
+    with open("./data/statistics_feature_0", "wb") as f:
+        pickle.dump(statistics_feature_0, f)
+    with open("./data/targets_1", "wb") as f:
+        pickle.dump(targets_1, f)
+    with open("./data/targets_0", "wb") as f:
+        pickle.dump(targets_0, f)
 
-    del index2word, sentence2index, statistics_feature, targets
+    del index2word, sentence2index_0, sentence2index_1, \
+        statistics_feature_0, statistics_feature_1, targets_0, targets_1
 
     word2vec = get_word2vec(word2index)
     with open("./data/word2vec", "wb") as f:
@@ -131,31 +154,39 @@ def preprocess():
 def getData():
     # 使用: 获取句子的词向量 [[vec11, vec12, ...], [vec21, vec22, ...], ...]
     # 未经padding
-    with open("./data/sentence2index", "rb") as f:
-        sentence2index = pickle.load(f)
+    with open("./data/sentence2index_1", "rb") as f:
+        sentence2index_1 = pickle.load(f)
+    with open("./data/sentence2index_0", "rb") as f:
+        sentence2index_0 = pickle.load(f)
     with open("./data/index2word", "rb") as f:
         index2word = pickle.load(f)
     with open("./data/word2vec", "rb") as f:
         word2vec = pickle.load(f)
 
-    sentenceVec = get_index2vec(sentence2index, index2word, word2vec)
+    #sentenceVec = get_index2vec(sentence2index, index2word, word2vec)
 
-    del sentence2index, index2word, word2vec
+    #del sentence2index, index2word, word2vec
 
     # 获取句子的target  [0, 1, ...]
-    with open("./data/targets", "rb") as f:
-        targets = pickle.load(f)
+    with open("./data/targets_1", "rb") as f:
+        targets_1 = pickle.load(f)
+    with open("./data/targets_0", "rb") as f:
+        targets_0 = pickle.load(f)
 
     # 获取statistic info  [[81, 32, 3, 2, 1, 45], [81, 32, 3, 2, 1, 42], ...]
     # 字符串长度 词的数量 大写字母数量 标点符号数量 特殊词数量 句子中token的数量(向量化词长度)
-    with open("./data/statistics_feature", "rb") as f:
-        statistics_feature = pickle.load(f)
+    with open("./data/statistics_feature_1", "rb") as f:
+        statistics_feature_1 = pickle.load(f)
+    with open("./data/statistics_feature_0", "rb") as f:
+        statistics_feature_0 = pickle.load(f)
 
-    return sentenceVec, targets, statistics_feature
+    return sentence2index_1, sentence2index_0, targets_1, targets_0, \
+           statistics_feature_1, statistics_feature_0, index2word, word2vec
 
 
 if __name__ == "__main__":
-    preprocess()
+    # preprocess()
     # 如果之前做过一次preprocess,之后都不需要调用preprocess,直接getData
-    sentenceVec, targets, statistics_feature = getData()
+    sentence2index_1, sentence2index_0, targets_1, targets_0, \
+    statistics_feature_1, statistics_feature_0, index2word, word2vec = getData()
     print("Finished!")
