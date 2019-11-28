a1d23be4a523adffd9a97731bcd111af07ea8551
Erebos <erebos@rams.colostate.edu>
2019-11-28 10:06:13 +0800

    fix prediction bug

--- model.py
+++ model.py
@@ -24,18 +24,16 @@ DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class quora_detector(nn.Module):
     def __init__(self, vec_dim):
         super(quora_detector, self).__init__()
-        self.rnn_1 = nn.LSTM(input_size = vec_dim, hidden_size = 64, batch_first = True, bidirectional = True, )
+        self.rnn_1 = nn.LSTM(input_size = vec_dim, hidden_size = 64, batch_first = True, bidirectional = True)
         self.rnn_2 = nn.LSTM(input_size = 128, hidden_size = 64, batch_first = True, bidirectional = True)
-        '''
+        
         self.conv = nn.Conv1d(in_channels=1, out_channels=64, kernel_size=1)
-        self.stat_dense = nn.Linear(fea_num, 64)
-        self.cated_dense_1 = nn.Linear(128, 256)
-        self.batchnorm_1 = nn.BatchNorm1d(256)
-        self.cated_dense_2 = nn.Linear(256, 128)
+        #self.stat_dense = nn.Linear(fea_num, 64)
+        self.cated_dense = nn.Linear(128, 128)
         self.dropout = nn.Dropout(p = 0.5)
-        self.batchnorm_2 = nn.BatchNorm1d(128)
-        '''
-        self.final_dense = nn.Linear(128, 1)
+        self.batchnorm = nn.BatchNorm1d(128)
+        
+        self.final_dense = nn.Linear(64, 1)
         self.initialize()
 
     # initialize with Xavier distribution
@@ -51,20 +49,23 @@ class quora_detector(nn.Module):
         nn.init.xavier_uniform_(w[1][0])
         nn.init.xavier_uniform_(w[1][1])
 
-    def forward(self, seq):
+    def forward(self, seq, length):
         # context embedding
-        # spatial dropout(0.5)
-        #seq = seq.permute(0,2,1)
-        #seq = F.dropout2d(seq, p = 0.5, training=True, inplace=False)
-        #seq = seq.permute(0,2,1)
-
-        #seq = seq[:,:30,:]
-        context, _ = self.rnn_1(seq)
-        context = F.relu(context)
-        context, _ = self.rnn_2(context)
-        context = context[:,-1,:]
-        context = F.relu(context)
         '''
+        # spatial dropout(0.5)
+        seq = seq.permute(0,2,1)
+        seq = F.dropout2d(seq, p = 0.5, training=True, inplace=False)
+        seq = seq.permute(0,2,1)
+        '''
+        #seq = torch.nn.utils.rnn.pack_padded_sequence(seq, length, batch_first = True)
+
+        context, _ = self.rnn_1(seq)
+        #context = self.batchnorm_1(context)
+        context, _ = self.rnn_2(context)
+        #context, _ = torch.nn.utils.rnn.pad_packed_sequence(context, batch_first = True)
+        #context = self.last_element(context, length)
+        context = self.batchnorm(context[:,-1,:])[:,:,None]
+        
         context = context.permute(0,2,1)
         # 1d convolution layer, 64 convolution kernels(to batchsize*64*128)
         context = self.conv(context)
@@ -72,7 +73,9 @@ class quora_detector(nn.Module):
         context = F.max_pool1d(context, kernel_size = context.shape[2:])
         context = context[:,:,-1]
         context = F.relu(context)
+        output = context
 
+        '''
         # stats features embedding(to batchsize*64)
         stat_features = self.stat_dense(stat_features)
         stat_features = F.relu(stat_features)
@@ -80,22 +83,17 @@ class quora_detector(nn.Module):
 
         # concat two embeddings(to batchsize*128)
         output = torch.cat((context, stat_features), 1)
+        '''
+        '''
         # full connection layer with dropout(0.5) and batchnorm layer(to batchsize*128)
-        output = self.cated_dense_1(output)
-        output = self.batchnorm_1(output)
-        output = F.relu(output)
-        #output = self.dropout(output)
-        output = self.cated_dense_2(output)
-        output = self.batchnorm_2(output)
+        output = self.cated_dense(context)
+        #output = self.batchnorm(output)
         output = F.relu(output)
         #output = self.dropout(output)
         '''
-
         # final output, 2 classes(to batchsize*2)
         output = self.final_dense(context)
         return output
-    
-
 '''
 # random generated test case
 x = torch.randn(32,35,300)
@@ -120,86 +118,98 @@ def text_to_array(text):
     embeds+= [empyt_emb] * (30 - len(embeds))
     return np.array(embeds)
 
-def predict(model, sentences, batch_size = 100, threshold = 0.5):
-    vec_dim = embeddings_index['how'].shape[0]
+def predict(model, sentences, batch_size = 100, threshold = 0.5, pred_bar = True):
+    sentences = np.array(sentences)
+    length = torch.tensor(list(map(len, sentences)))
     ret = np.array([])
-    for k in tqdm(range((len(sentences)-1)//batch_size + 1)):
-        batch_sentences = sentences[k*batch_size:(k+1)*batch_size]
+    iterator = tqdm(range((len(sentences)-1)//batch_size + 1)) if pred_bar == True else range((len(sentences)-1)//batch_size + 1)
+    for k in iterator:
+        batch_L = length[k*batch_size:(k+1)*batch_size]
+        batch_L, idx = torch.sort(batch_L, descending = True)
+        batch_L = batch_L.to(DEVICE)
+
+        batch_sentences = sentences[k*batch_size:(k+1)*batch_size][idx]
+        
         batch_X = torch.tensor([text_to_array(sentence) for sentence in batch_sentences]).float().to(DEVICE)
+        #batch_X = torch.nn.utils.rnn.pad_sequence(batch_X, batch_first=True).float().to(DEVICE)
 
-        logit = model.forward(batch_X)
-        prob = prob = torch.sigmoid(logit)[:,0]
+        logit = model.forward(batch_X, batch_L)[:,0]
 
-        res = np.where(prob.cpu() > threshold, 1, 0)
+        prob = prob = torch.sigmoid(logit)
+        
+        # recover the order the data
+        _, rev_idx = torch.sort(idx)
+        res = torch.tensor(np.where(prob.cpu() > threshold, 1, 0))[rev_idx]
         ret = np.r_[ret, res]
     return ret
 
-def train(model, train_df, val_df, learning_rate = 0.001, batch_size = 100, optimizer = "Adam", iterations = 100, threshold = 0.5, training = True):
-    vec_dim = embeddings_index['how'].shape[0]
+def train(model, train_df, val_df, learning_rate = 0.001, batch_size = 100, optimizer = "Adam", iterations = 100, threshold = 0.5, setp_per_epoch = 1000):
     train_sentences = np.array(train_df['question_text'])
-    train_targets = np.array(train_df['target'])
-    
-    if training:
-        if optimizer == "SGD":
-            optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
-        else:
-            optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+    train_targets = torch.tensor(np.array(train_df['target']))
+    train_length = torch.tensor(list(map(len, train_sentences)))
 
-        # put model on GPU and parallelize
-        model.to(DEVICE)
-        #model = nn.DataParallel(model)
+    if optimizer == "SGD":
+        optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+    else:
+        optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
 
-        print("Training will on GPU" if DEVICE == torch.device("cuda:0") else "Training will on CPU")
+    # put model on GPU and parallelize
+    model.to(DEVICE)
+    #model = nn.DataParallel(model)
 
-        losses = []
-        #train_errors = []
-        #test_errors = []
+    print("Training will on GPU" if DEVICE == torch.device("cuda:0") else "Training will on CPU")
 
-        model.train()
-        criterion = nn.BCEWithLogitsLoss()
-        train_start_time = time.perf_counter()
-        for epoch in range(iterations):
-            idx = np.random.permutation(train_sentences.shape[0])
-            train_sentences = train_sentences[idx]
-            train_targets = train_targets[idx]
-            #step_per_epoch = (len(train_sentences)-1)//batch_size + 1
-            with tqdm(total = 1000, desc='epoch {}'.format(epoch+1), ncols=80) as t:
-                for k in range(1000):
-                    # free unreferenced cuda memory and put gradient zero
-                    torch.cuda.empty_cache()
-                    optimizer.zero_grad()
+    losses = []
+    #train_errors = []
+    #test_errors = []
 
-                    batch_sentences = train_sentences[k*batch_size:(k+1)*batch_size]
-                    
-                    batch_X = torch.tensor([text_to_array(sentence) for sentence in batch_sentences]).float().to(DEVICE)
-                    batch_Y = torch.tensor(train_targets[k*batch_size:(k+1)*batch_size]).float().to(DEVICE)
+    model.train()
+    criterion = nn.BCEWithLogitsLoss()
+    train_start_time = time.perf_counter()
+    val_show = val_df[:3000]
+    for epoch in range(iterations):
+        idx = np.random.permutation(train_sentences.shape[0])
+        train_sentences = train_sentences[idx]
+        train_targets = train_targets[idx]
+        step_per_epoch = min(setp_per_epoch, (len(train_sentences)-1)//batch_size + 1)
+        with tqdm(total = setp_per_epoch, desc='epoch {}'.format(epoch+1)) as t:
+            for k in range(setp_per_epoch):
+                # free unreferenced cuda memory and put gradient zero
+                torch.cuda.empty_cache()
+                optimizer.zero_grad()
 
-                    logit = model.forward(batch_X)[:,0]
-                    loss = criterion(logit, batch_Y)
+                batch_L = train_length[k*batch_size:(k+1)*batch_size]
+                batch_L, idx = torch.sort(batch_L, descending = True)
+                batch_L = batch_L.to(DEVICE)
 
-                    loss.backward()
-                    optimizer.step()
+                batch_sentences = train_sentences[k*batch_size:(k+1)*batch_size][idx]
+                
+                batch_X = torch.tensor([text_to_array(sentence) for sentence in batch_sentences]).float().to(DEVICE)
+                #batch_X = torch.nn.utils.rnn.pad_sequence(batch_X, batch_first=True).float().to(DEVICE)
+                batch_Y = train_targets[k*batch_size:(k+1)*batch_size][idx].float().to(DEVICE)
 
-                    losses.append(float(loss))
-                    prediction_train = predict(model, train_df.iloc[:10000]['question_text'], batch_size = 512)
-                    prediction_val = predict(model, val_df.iloc[:3000]['question_text'], batch_size = 512)
-                    f1_train = f1_score(train_df.iloc[:3000]['target'], prediction_train)
-                    f1_val = f1_score(val_df.iloc[:10000]['target'], prediction_val)
-                    t.set_postfix(loss='{:^7.3f}, f1_train = {:^7.3f}, f1_val = {:^7.3f}'.format(loss, f1_train, f1_val))
-                    t.update()
-            #print("last loss in epoch {}: {}".format(epoch+1, loss))
-        train_stop_time = time.perf_counter()
-        print("Total Training Time: {} seconds".format(train_stop_time - train_start_time))
+                logit = model.forward(batch_X, batch_L)[:,0]
+                loss = criterion(logit, batch_Y)
 
-        # save model into binary file
-        with open("./model", 'wb') as f:
-            pickle.dump(model, f)
+                loss.backward()
+                optimizer.step()
 
-        # show and save loss-iteration plot
-        plt.plot(range(1, 1 + len(losses)), losses)
-        plt.xlabel("iterations")
-        plt.ylabel("loss function")
-        plt.savefig("loss.jpg")
+                losses.append(float(loss))
+                t.set_postfix(loss='{:^7.3f}'.format(loss))
+                t.update()
+        #print("last loss in epoch {}: {}".format(epoch+1, loss))
+    train_stop_time = time.perf_counter()
+    print("Total Training Time: {} seconds".format(train_stop_time - train_start_time))
+
+    # save model into binary file
+    with open("./model", 'wb') as f:
+        pickle.dump(model, f)
+
+    # show and save loss-iteration plot
+    plt.plot(range(1, 1 + len(losses)), losses)
+    plt.xlabel("iterations")
+    plt.ylabel("loss function")
+    plt.savefig("loss.jpg")
 
 if __name__ == "__main__":
     VALID_SAMPLE_RATE = 0.1
@@ -214,14 +224,20 @@ if __name__ == "__main__":
     #val_df = val_df.iloc[:1000]
 
     for th in [0.5]:
-        #with open("./model", 'rb') as f:
+        
         model = quora_detector(embeddings_index['how'].shape[0])
         train(model, train_df, val_df, \
-            learning_rate = 0.0005, batch_size = 128, optimizer = "Adam", iterations = 20, threshold = th, training=True)
+            learning_rate = 0.0001, batch_size = 128, optimizer = "Adam", iterations = 20, threshold = th, setp_per_epoch = 1000)
         
+        '''
+        with open("./model", 'rb') as f:
+            model = pickle.load(f)
+        '''
+        model.eval()
         # Final test
-        test_df = pd.read_csv("data/train_split")
+        test_df = pd.read_csv("data/test_split.csv")
         prediction = predict(model, test_df['question_text'], batch_size = 512)
+        print("Evaluation loading")
         acc = accuracy_score(test_df['target'], prediction)
         f1 = f1_score(test_df['target'], prediction)
         print("Accuracy: {}".format(acc))
